4bc5d2d94decd804671b39451db7f85c935f8d4b
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-03-18 01:32:03 +0200

    add calendar update

--- Collaboration Messenger/src/views/MyCalendar/MyCalendar.tsx
+++ Collaboration Messenger/src/views/MyCalendar/MyCalendar.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Calendar, momentLocalizer, SlotInfo } from 'react-big-calendar';
+import { Calendar, momentLocalizer, SlotInfo ,Views} from 'react-big-calendar';
 import moment from 'moment';
-import Modal from 'react-modal';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './MyCalendar.css';
 import { getDatabase, onValue, push, ref } from 'firebase/database';
 import { useAppContext } from '../../context/appContext';
-import UserSearch from '../../components/Search/UserSearch';
 import { getAllUsers } from '../../service/user';
 import { useNavigate } from 'react-router-dom';
+
 
 const localizer = momentLocalizer(moment);
 
@@ -19,30 +18,37 @@ interface Event {
   sharedWith?: string[]; // Add sharedWith field to Event interface
 }
 
+interface Group {
+  id: string;
+  username: string;
+  members: string[];
+}
+
 const MyCalendar: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedSlot, setSelectedSlot] = useState<SlotInfo | null>(null);
-  const [title, setTitle] = useState('');
+  const [isUserListVisible, setIsUserListVisible] = useState(false);
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+  const [selectedGroup, setSelectedGroup] = useState<string>('');//group
+  const [groups, setGroups] = useState<Group[]>([]);//group
   const [start, setStart] = useState(new Date().toISOString());
   const [end, setEnd] = useState(new Date().toISOString());
-  const [attendees, setAttendees] = useState([]);
-  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isUserListVisible, setIsUserListVisible] = useState(false);
+  const [attendees, setAttendees] = useState([]);
+  const [users, setUsers] = useState<any>([]);
+  const [title, setTitle] = useState('');
   const { userData } = useAppContext();
   const navigate = useNavigate();
-  
-  const [users, setUsers] = useState<any>([]);
 
+  //users 
   useEffect(() => {
     const fetchUsers = async () => {
       const snapshot = await getAllUsers();
       setUsers(snapshot);
     }
-
     fetchUsers();
   }, []);
+
 
   useEffect(() => {
     const db = getDatabase();
@@ -63,28 +69,58 @@ const MyCalendar: React.FC = () => {
     });
   }, [userData]);
 
+  //group
+  useEffect(() => {
+    const db = getDatabase();
+    const userGroupsRef = ref(db, `users/${userData?.username}/groups`);
+    onValue(userGroupsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const userGroups = Object.keys(data).map((key) => {
+          return {
+            id: key,
+            ...data[key]
+          }
+        });
+        setGroups(userGroups);
+      } else {
+        setGroups([]);
+      }
+    });
+  }, [userData]);
+
   const handleSelect = (slotInfo: SlotInfo) => {
     console.log('handleSelect called', slotInfo);
     setIsModalOpen(true);// Open the modal when a slot is selected
     setSelectedSlot(slotInfo);
     setStart(slotInfo.start.toISOString());
     setEnd(slotInfo.end.toISOString());
-    
+
   };
 
+  //user select
   const handleUserSelect = (userId: string) => {
     setSelectedUsers([userId]);
   };
+  //group select
+  const handleGroupSelect = (groupId: string) => {
+    setSelectedGroup(groupId);
+    const group = groups.find(group => group.id === groupId);
+    if (group) {
+      setSelectedUsers(group.members);
+    }
+  };
+
   const handleSave = () => {
     const db = getDatabase();
     const newEvent = {
       title,
-      start, // Use the start time of the selected slot
-      end, // Use the end time of the selected slot
+      start,
+      end,
       attendees,
-      sharedWith: selectedUsers // Include selected users/group in the event 
+      sharedWith: selectedUsers
     };
-  
+
     // Save the event to the 'events' node
     push(ref(db, `users/${userData?.username}/events`), newEvent)
       .then(() => {
@@ -100,7 +136,8 @@ const MyCalendar: React.FC = () => {
       .catch((error) => {
         console.error('Error saving event: ', error);
       });
-  
+
+    // Save the event to the selected group's members' nodes
     // Save the event to the selected user's node
     selectedUsers.forEach((userId) => {
       push(ref(db, `users/${userId}/events`), newEvent)
@@ -111,6 +148,8 @@ const MyCalendar: React.FC = () => {
           console.error(`Error saving event in user ${userId}'s database: `, error);
         });
     });
+
+  
   };
 
   const closeModal = () => {
@@ -124,18 +163,17 @@ const MyCalendar: React.FC = () => {
 
 
   // const handleSelectEvent = (event: Event) => {
-  //   // Find the chat that corresponds to the user
-  //   const chat = chats.find(chat => chat.uid === event.sharedWith[0]);
-  
+
+
   //   if (chat) {
   //     // If the chat is found, navigate to the chat with the user
-  //     navigate(`/privateChats/${chat.id}`);
+  //     navigate(`/privateChats/${priva.id}`);
   //   } else {
   //     // If the chat is not found, handle the error (e.g., show an error message)
   //     console.error('Chat not found');
   //   }
   // };
-
+ 
   return (
     <>
       <div className="my-calendar-container">
@@ -145,34 +183,46 @@ const MyCalendar: React.FC = () => {
           defaultView="month"
           events={events}
           onSelectSlot={handleSelect}
+          views={['month', 'day', 'agenda']} // Only these views will be enabled
           // onSelectEvent={handleSelectEvent} // Add this line
           selectable
           className="my-calendar"
         />
       </div>
       <div>
-        {isModalOpen && (
-          <div className="modal">
-            <h2>New Event</h2>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <button onClick={handleSave}>Save Event</button>
-            <button onClick={closeModal}>Cancel</button>
+  {isModalOpen && (
+    <div className="modal">
+      <h2>New Event</h2>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button onClick={handleSave}>Save Event</button>
+      <button onClick={closeModal}>Cancel</button>
 
-            <div>
-              <h3>Share with:</h3>
-
-              <select value={selectedUsers[0] || ''} onChange={(e) =>
-                 handleUserSelect(e.target.value)}>
-                {users.map((user: any) => (
-                  <option key={user.id} value={user.id}>
-                    {user.username}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-        )}
+      <div>
+        <h3>Share with:</h3>
+        <select value={selectedUsers[0] || ''} onChange={(e) =>
+          handleUserSelect(e.target.value)}>
+          {users.map((user: any) => (
+            <option key={user.id} value={user.id}>
+              {user.username}
+            </option>
+          ))}
+        </select>
       </div>
+
+      <div>
+        <h3>Select group:</h3>
+        <select value={selectedGroup || ''} onChange={(e) =>
+          handleGroupSelect(e.target.value)}>
+          {groups.map((group: any) => (
+            <option key={group.id} value={group.id}>
+              {group.username}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )}
+</div>
     </>
   );
 };
