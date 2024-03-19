1ae7433b7efb91cb360b7ede0931f6acd1cd7f34
YordanAleksandrov <dani541112@gmail.com>
2024-03-19 19:08:02 +0200

    chat css

--- Collaboration Messenger/src/components/chat/MessageContent.tsx
+++ Collaboration Messenger/src/components/chat/MessageContent.tsx
@@ -18,14 +18,14 @@ export type Message = {
     type: string,
     files: File[],
     id: string,
-    replyMessage?:string
+    replyMessage?: string
 }
 export type File = {
     url: string,
     type: string,
     name: string
 }
-export default function MessageContent({ message, type,setReplyMessage }: { message: Message, type: string,setReplyMessage?:React.Dispatch<React.SetStateAction<string>> }) {
+export default function MessageContent({ message, type, setReplyMessage }: { message: Message, type: string, setReplyMessage?: React.Dispatch<React.SetStateAction<string>> }) {
 
     const [files, setFiles] = useState<File[]>([]);
     const { userData } = useAppContext();
@@ -39,9 +39,9 @@ export default function MessageContent({ message, type,setReplyMessage }: { mess
         if (message.type === 'image' || message.type === 'file') {
             setFiles(message.files);
         }
-        message.reactions? setReaction(Object.keys(message.reactions).map((key) => {
+        message.reactions ? setReaction(Object.keys(message.reactions).map((key) => {
             return { emoji: key, users: Object.keys(message.reactions[key]) }
-        })):[];
+        })) : [];
 
     }, [message])
 
@@ -67,84 +67,100 @@ export default function MessageContent({ message, type,setReplyMessage }: { mess
             id && editGroupMessage(id, message, editValue);
         }
     }
-    const addCurrentEmojiToMessage = (emoji: string,message:Message) => {
-       
+    const addCurrentEmojiToMessage = (emoji: string, message: Message) => {
+
         if (type === 'private') {
-            id && AddEmojiToMessage(id,message,emoji,userData.username);
+            id && AddEmojiToMessage(id, message, emoji, userData.username);
         } else {
-            id && AddEmojiToGroupMessage(id,message,emoji,userData.username);
+            id && AddEmojiToGroupMessage(id, message, emoji, userData.username);
         }
-        
+
     }
 
     return (
-        edit ?
-            <div className="message-content">
-                <input type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)} />
-                <button onClick={() => { saveCurrentEdit(message) }}>Save</button>
-                <button onClick={() => setEdit(false)}>Cancel</button>
-            </div>
-            :
-            <div onMouseOver={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                className="message-content">
-                {message.replyMessage&&<h5>{message.replyMessage}</h5>}
-                <p>{message.content}</p>
-                {files.map((f, i) => {
-                    return (
-                        <div key={i}>
-                            {f.type === 'image' ? <img className="message-image-file" src={f.url} alt="image" />
-                                :
-                                <div className="file-display">
-                                    <img className="message-image-file" src={"/image/empty.file.png"} alt="image" />
-                                    <a href={f.url} download>{f.name}</a>
-                                </div>}
+        <>
+            {edit ?
+                <div className="message-content">
+                    <input type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)} />
+                    <button onClick={() => { saveCurrentEdit(message) }}>Save</button>
+                    <button onClick={() => setEdit(false)}>Cancel</button>
+                </div>
+                :
+                <div onMouseOver={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    className="message-content">
+                    <div id="message-reply-container">
+                        <div id="main-message-container">
+                            {message.replyMessage && <h5>{message.replyMessage}</h5>}
                         </div>
-                    );
-                })}
-                <div className="message-reactions">
-                    {reaction.map((r:any, i:number) => {
-                        return <div key={i}> {r.users.length>0&&( <div key={i} className="reaction">
-                            <span className='emodji-reaction' onClick={()=>
-                                {addCurrentEmojiToMessage(r.emoji,message)
-                                    if(r.users.includes(userData.username)){
-                                        r.users = r.users.filter((u:string)=>u!==userData.username);
+                        <div id="reply-container">
+                            {message.replyMessage ? <h5 style={{
+                                marginTop: "20px",
+                            }}>Reply:{message.content}</h5> : <h5 style={{
+                                margin: 0,
+                            }}>{message.content}</h5>}
+                        </div>
+                    </div>
+                    {files.map((f, i) => {
+                        return (
+                            <div key={i}>
+                                {f.type === 'image' ?
+                                    <div id="image-image-text-container">
+                                        <img className="message-image-file" src={f.url} alt="image" />
+                                        <a href={f.url} download>{f.name}</a>
+                                    </div>
+                                    :
+                                    <div className="file-display">
+                                        <img className="message-image-file" src={"/image/empty.file.png"} alt="image" />
+                                        <a href={f.url} download>{f.name}</a>
+                                    </div>}
+                            </div>
+                        );
+                    })}
+                    <div className="message-reactions">
+                        {reaction.map((r: any, i: number) => {
+                            return <div key={i}> {r.users.length > 0 && (<div key={i} className="reaction">
+                                <span className='emodji-reaction' onClick={() => {
+                                    addCurrentEmojiToMessage(r.emoji, message)
+                                    if (r.users.includes(userData.username)) {
+                                        r.users = r.users.filter((u: string) => u !== userData.username);
                                     }
-                                    else{
+                                    else {
                                         r.users.push(userData.username);
                                     }
                                 }}>{r.emoji}</span>
-                           {r.users.length>1&& <span>{r.users.length}</span>}
-                        </div>)}
+                                {r.users.length > 1 && <span>{r.users.length}</span>}
+                            </div>)}
+                            </div>
+                        })}
+                    </div>
+                    {showEmojiPicker &&
+                        <div className={'emoji-picker'} >
+                            <button onClick={() => { setShowEmojiPicker(false) }}>X</button>
+                            <Picker
+                                onEmojiClick={(emojiObject) => {
+                                    if (emojiObject) {
+                                        addCurrentEmojiToMessage(emojiObject.emoji, message);
+                                        setShowEmojiPicker(false);
+                                    }
+                                }}
+                            />
+
                         </div>
-                    })}
-                </div>
-                {showEmojiPicker && 
-                    <div className={'emoji-picker'} >
-                    <button onClick={()=>{setShowEmojiPicker(false)}}>X</button>
-                        <Picker
-                            onEmojiClick={(emojiObject) => {
-                                if (emojiObject) {
-                                    addCurrentEmojiToMessage(emojiObject.emoji,message);
-                                    setShowEmojiPicker(false);
-                                }
-                            }}
-                        />
-                       
-                    </div>
-                }
-                {isHovered && 
-                    <div className="message-options">
-                        {userData.username === message.author && <button onClick={() => deleteCurrentMessage(message.id)}><DeleteIcon/></button>}
-                        {userData.username === message.author && <button onClick={() => {
-                            setEdit(true);
-                            setEditValue(message.content);
-                        }}><EditNoteIcon/></button>}
-                        <button onClick={() => setShowEmojiPicker(true)}><AddReactionIcon/></button>
-                        {<button onClick={() => setReplyMessage&&setReplyMessage(message.content)}><ReplyAllIcon/></button>}
-                        
-                    </div>
-                }
-            </div>
+                    }
+                    {isHovered &&
+                        <div className="message-options">
+                            {userData.username === message.author && <button onClick={() => deleteCurrentMessage(message.id)}><DeleteIcon /></button>}
+                            {userData.username === message.author && <button onClick={() => {
+                                setEdit(true);
+                                setEditValue(message.content);
+                            }}><EditNoteIcon /></button>}
+                            <button onClick={() => setShowEmojiPicker(true)}><AddReactionIcon /></button>
+                            {<button onClick={() => setReplyMessage && setReplyMessage(message.content)}><ReplyAllIcon /></button>}
+
+                        </div>
+                    }
+                </div>}
+        </>
     );
 }
