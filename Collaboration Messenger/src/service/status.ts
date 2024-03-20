2cffa5ef17d2d18ab86499637c6b768fdce0311c
YordanAleksandrov <dani541112@gmail.com>
2024-03-20 09:06:08 +0200

    bug

--- Collaboration Messenger/src/service/status.ts
+++ Collaboration Messenger/src/service/status.ts
@@ -10,60 +10,60 @@ export const toggleStatus = async (userData: any) => {
     return;
   }
   const groups = userData.groups ? Object.keys(userData.groups) : [];
-  const friends = userData.friends ? Object.keys(userData.friends) : [];
+  // const friends = userData.friends ? Object.keys(userData.friends) : [];
   update(ref(db, `users/${userData.username}`), { status: 'online' });
   onDisconnect(ref(db, `users/${userData.username}`)).update({ status: 'offline' });
   groups.forEach(id => {
     update(ref(db, `groups/${id}/members/${userData.username}`), { status: 'online' });
     onDisconnect(ref(db, `groups/${id}/members/${userData.username}`)).update({ status: 'offline' });
   });
-  friends.forEach(friend => {
-    update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'online' });
-    onDisconnect(ref(db, `users/${friend}/friends/${userData.username}`)).update({ status: 'offline' });
-  });
+  // friends.forEach(friend => {
+  //   update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'online' });
+  //   onDisconnect(ref(db, `users/${friend}/friends/${userData.username}`)).update({ status: 'offline' });
+  // });
 };
 export const setStatusToBusy = async (userData: any) => {
   if (!userData.username) {
     return;
   }
   const groups = userData.groups ? Object.keys(userData.groups) : [];
-  const friends = userData.friends ? Object.keys(userData.friends) : [];
+  // const friends = userData.friends ? Object.keys(userData.friends) : [];
   update(ref(db, `users/${userData.username}`), { status: 'busy' });
   onDisconnect(ref(db, `users/${userData.username}`)).update({ status: 'offline' });
   groups.forEach(id => {
     update(ref(db, `groups/${id}/members/${userData.username}`), { status: 'busy' });
     onDisconnect(ref(db, `groups/${id}/members/${userData.username}`)).update({ status: 'offline' });
   });
-  friends.forEach(friend => {
-    update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'busy' });
-    onDisconnect(ref(db, `users/${friend}/friends/${userData.username}`)).update({ status: 'offline' });
-  });
+  // friends.forEach(friend => {
+  //   update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'busy' });
+  //   onDisconnect(ref(db, `users/${friend}/friends/${userData.username}`)).update({ status: 'offline' });
+  // });
 };
 export const changeStatusToAway = async (userData: any) => {
   if (!userData.username) {
     return;
   }
   const groups = userData.groups ? Object.keys(userData.groups) : [];
-  const friends = userData.friends ? Object.keys(userData.friends) : [];
+  // const friends = userData.friends ? Object.keys(userData.friends) : [];
   update(ref(db, `users/${userData.username}`), { status: 'away' });
   groups.forEach(id => {
     update(ref(db, `groups/${id}/members/${userData.username}`), { status: 'away' });
   });
-  friends.forEach(friend => {
-    update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'away' });
-  });
+  // friends.forEach(friend => {
+  //   update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'away' });
+  // });
 };
 export const updateStatusToOnline = async (userData: any) => {
   if (!userData.username) {
     return;
   }
   const groups = userData.groups ? Object.keys(userData.groups) : [];
-  const friends = userData.friends ? Object.keys(userData.friends) : [];
+  // const friends = userData.friends ? Object.keys(userData.friends) : [];
   update(ref(db, `users/${userData.username}`), { status: 'online' });
   groups.forEach(id => {
     update(ref(db, `groups/${id}/members/${userData.username}`), { status: 'online' });
   });
-  friends.forEach(friend => {
-    update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'online' });
-  });
+  // friends.forEach(friend => {
+  //   update(ref(db, `users/${friend}/friends/${userData.username}`), { status: 'online' });
+  // });
 };
